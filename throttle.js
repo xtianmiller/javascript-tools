da34c60bfcfc2eea153c45136895fba68c277443
Christian Miller <xtianmiller@users.noreply.github.com>
2016-02-12 11:35:27 -0500

    Update throttle.js

--- throttle.js
+++ throttle.js
@@ -13,9 +13,47 @@ var throttle = function(callback, limit) {
     };
 };
 
+// More elaborate Underscore method
+function throttle(func, wait, options) {
+    var _ = {
+        now: Date.now || function() {
+            return new Date().getTime();
+        }
+    }
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    if (!options) options = {};
+    var later = function() {
+        previous = options.leading === false ? 0 : _.now();
+        timeout = null;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+    };
+    return function() {
+        var now = _.now();
+        if (!previous && options.leading === false) previous = now;
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0 || remaining > wait) {
+            if (timeout) {
+                clearTimeout(timeout);
+                timeout = null;
+            }
+            previous = now;
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+        } else if (!timeout && options.trailing !== false) {
+            timeout = setTimeout(later, remaining);
+        }
+        return result;
+    };
+};
+
 // Usage
 var myEfficientFn = throttle(function() {
-	// All the taxing stuff you do
+    // All the taxing stuff you do
 }, 250);
 
 window.addEventListener('scroll', myEfficientFn);
